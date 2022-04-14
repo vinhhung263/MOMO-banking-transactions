5cb54956db13fa0adc6b856af62c6e3d6f08ccaa
Frank Gaston <62415557+vinhhung263@users.noreply.github.com>
2022-04-14 09:12:00 +0700

    Update addBankTransfer.php

--- addBankTransfer.php
+++ addBankTransfer.php
@@ -1,55 +1,57 @@
 /*
- Notice : I use 3 tables
- - member : store member information and balance(wallet/point..)
- - callbacklog : store every callback log
- - balancelog : store every changes of balance
+Notice : I use 3 tables
+- member : store member information and balance(wallet/point..)
+- callbacklog : store every callback log
+- balancelog : store every changes of balance
 */
 
 <?php
+
 $now = date('Y-m-d H:i:s');
 $postdata = @$_POST; // $_POST is $dataInsert from banktransfer.php
 
 foreach ($postdata as &$item) {
-	$item = $mysql->escape($item);
+    $item = $mysql->escape($item);
 }
 
 //Ex: Rule about description is "<brandname> <customer account>"
 if (strpos($postdata['description'], '<brandname>') !== false) {
-	$query = "SELECT id FROM callbacklog WHERE transaction_id = '".$postdata['transaction_id']."'";
-	$dataQueryCallbackLog = $mysql->query($query)->row;
+    $query = "SELECT id FROM callbacklog WHERE transaction_id = '" . $postdata['transaction_id'] . "'";
+    $dataQueryCallbackLog = $mysql->query($query)->row;
 
-	if(empty($dataQueryCallbackLog)){
-		$description = $postdata['description'];
-		$amount = $postdata['amount'];
-		$createDate = $postdata['create_date'];
+    if (empty($dataQueryCallbackLog)) {
+        $description = $postdata['description'];
+        $amount = $postdata['amount'];
+        $createDate = $postdata['create_date'];
 
-		//Find customer account based on description "<brandname> <customer account>" 
-		//(customer account is email account)
-		$account = explode(' ',$description)[1] . '@';
-		$query = "SELECT memberid,balance FROM member WHERE memberemail LIKE '%$account%'";
-		$data_query_member = $mysql->query($query)->row;
+        //Find customer account based on description "<brandname> <customer account>" 
+        //(customer account is email account)
+        $account = explode(' ', $description)[1] . '@';
+        $query = "SELECT memberid,balance FROM member WHERE memberemail LIKE '%$account%'";
+        $dataQueryMember = $mysql->query($query)->row;
 
-		if(!empty($data_query_member)){
-			$current_balance = $data_query_member['balance'];
-			$new_balance = $current_balance + $amount;
-			$memberid = $data_query_member['memberid'];
+        if (!empty($dataQueryMember)) {
+            $currentBalance = $dataQueryMember['balance'];
+            $newBalance = $currentBalance + $amount;
+            $memberid = $dataQueryMember['memberid'];
 
-            $query = "UPDATE member SET balance = '$new_balance' WHERE memberid = '$memberid'";
-			$mysql->query($query);
+            $query = "UPDATE member SET balance = '$newBalance' WHERE memberid = '$memberid'";
+            $mysql->query($query);
 
-			$dataInsert = array(
-				'memberid' => $memberid,
-				'currentbalance' => $current_balance,
-				'newbalance' => $new_balance,
-				'reason' => "Receive from " .$postdata['service_source'],
-				'createddate' => $createDate,
-				'addeddate' => $createDate,
-				'createdby' => 'callback',
-				'addedby' => 'callback',
-			);
-			$mysql->forminsert('balancelog',$dataInsert); //insert data to table balancelog
-		}
-	}
-	$mysql->forminsert('callbacklog',$postdata); //insert log
+            $dataInsert = array(
+                'memberid' => $memberid,
+                'currentbalance' => $currentBalance,
+                'newbalance' => $newBalance,
+                'reason' => "Receive from " . $postdata['service_source'],
+                'createddate' => $createDate,
+                'addeddate' => $createDate,
+                'createdby' => 'callback',
+                'addedby' => 'callback',
+            );
+            $mysql->forminsert('balancelog', $dataInsert); //insert data to table balancelog
+        }
+    }
+    $mysql->forminsert('callbacklog', $postdata); //insert log
 }
+
 ?>
